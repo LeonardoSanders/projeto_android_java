819da4364931192e8eb9b8af6097dd2c16f27a9a
Leonardo Sanders <leonardosanders54@gmail.com>
2024-07-26 14:45:51 -0400

    Adicionei a parte de operadores condicionais

--- projeto android/src/estruturas_condicionais.java
+++ projeto android/src/estruturas_condicionais.java
@@ -1,5 +1,30 @@
 public class estruturas_condicionais {
     public static void main(String[] args) {
         
+        int idade = 20;
+
+        if (idade <= 10) {
+
+            System.out.println("Criança");
+
+        }else if (idade <= 17) {
+
+            System.out.println("Adolescente");
+
+        }else{
+            
+            System.out.println("Adulto");
+        }
+
+        //String sexo = "";
+        
+        //if (sexo == "masculino"){
+        //    System.out.println("Sexo Masculino");
+        //}else if (sexo == "feminino"){
+        //    System.out.println("Sexo Feminino");
+        //}else{
+        //    System.out.println("Sexo não definido");
+        //}
+    
     }
 }
