1b6b2fcd911e08a3798f38a31f4292a5da5767f1
Leonardo Sanders <leonardosanders54@gmail.com>
2024-07-26 11:32:40 -0400

    Integração inicial

---
+++ projeto android/src/tipos_de_variaveis.java
@@ -0,0 +1,19 @@
+public class tipos_de_variaveis {
+    public static void main(String[] args) throws Exception {
+        
+        String nome = "Leonardo Sanders";
+        byte idade = 29;
+        int numero_RG = 25120123;
+        double media = 4.5;
+        float nota = 9.75f;
+        boolean sexo = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(numero_RG);
+        System.out.println(media);
+        System.out.println(nota);
+        System.out.println(sexo);
+
+    }
+}
