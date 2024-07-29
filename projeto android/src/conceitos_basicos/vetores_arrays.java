8c18759ec66434b2e1ecc83b52b6b7d8542b4978
Leonardo Sanders <leonardosanders54@gmail.com>
2024-07-29 14:12:44 -0400

    Criação da pasta Conceitos Básicos

--- projeto android/src/conceitos_basicos/vetores_arrays.java
+++ projeto android/src/conceitos_basicos/vetores_arrays.java
@@ -2,7 +2,9 @@ public class vetores_arrays {
     public static void main(String[] args) {
         
         String[] lista_nome = {"Pedro", "Leonardo", "Luis"};
+        int[] numeros = {10, 20, 30, 40, 50};
 
-        System.out.println(lista_nome[3]);
+        System.out.println(lista_nome[2]);
+        System.out.println(numeros[1]);
     }
 }
